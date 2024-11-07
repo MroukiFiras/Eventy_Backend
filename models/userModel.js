9939cf983c41b9d722c1abf2e35ea9c988f008cd
MroukiFiras <114618223+MroukiFiras@users.noreply.github.com>
2024-11-07 22:37:13 +0100

    The last commit for today

---
+++ models/userModel.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  address: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  profileImage: {
+    type: String,
+    default:
+      "https://res.cloudinary.com/db8sek2rg/image/upload/v1731014068/defaultAvatar.jpg",
+  },
+  hasVerifiedEmail: {
+    type: Boolean,
+    default: false,
+  },
+  userRole: {
+    user: { type: Boolean, default: true },
+    admin: { type: Boolean, default: false },
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
